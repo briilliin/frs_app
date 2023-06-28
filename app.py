9b0563b1153377318a5d13cd8a37005739ec5aca
antonfominov <61196201+antonfominov@users.noreply.github.com>
2023-06-28 13:54:33 +0300

    Добавляем запросы к frs

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ import os
 
 import psycopg2
 from flask import Flask, redirect
-from flask import render_template, request
-from data.OperationsStudents import OperationsStudents
+from flask import render_template, request, Response, send_from_directory
+import cv2
+import requests
+import json
+# from data.OperationsStudents import OperationsStudents
 
 app = Flask(__name__)
 db_name = 'frs_db'
@@ -11,6 +14,20 @@ user = "postgres"
 password = "123"
 host = "localhost"
 port = "5432"
+
+
+def gen_frames():  # generate frame by frame from camera
+    camera = cv2.VideoCapture(0)
+    while True:
+        # Capture frame-by-frame
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -22,47 +39,63 @@ def hello_world():
             surname = request.form.get('surname')
             middle_name = request.form.get('middle_name')
             group = int(request.form.get('group'))
-            student = OperationsStudents()
-            student.create_student(name=student_name, surname=surname, middle_name=middle_name, group=group)
-
-            data = student.get_all()
-            return render_template("index.html", data=data)
+            return render_template('index.html')
         except AssertionError:
             return render_template('index.html')
     else:
-        student = OperationsStudents
-        data = student.get_all(student)
-        return render_template("index.html", data=data)
+        return render_template('index.html')
 
 
 @app.route('/take_photo', methods=['POST', 'GET'])
 def take_photo():
-    # student_name = request.form.get('name')
-    # surname = request.form.get('surname')
-    # middle_name = request.form.get('middle_name')
-    # group = int(request.form.get('group'))
-    # student = OperationsStudents()
-    # student.create_student(name=student_name, surname=surname, middle_name=middle_name, group=group)
-    #
-    # data = student.get_all()
+
     print(request.form.get('imageURL'))
     return render_template("camera.html")
 
+@app.route('/add_stream')
+def add_stream():
+    payload = dict(streamId='1', url='http://127.0.0.1:5000/video_feed', callback="http://127.0.0.1:5000/faceRecognized")
+    json = '{"streamId":"1","url":"http://127.0.0.1:5000/video_feed","callback":"http://127.0.0.1:5000/faceRecognized"}'
+    r = requests.post('http://localhost:9051/api/addStream', data=json)
+    print(payload)
+    return Response(r)
 
-#
-# @app.route('/', methods=['POST', 'GET'])
-# def registration():
-#     try:
-#         student = OperationsStudents()
-#         name = request.form.get('name')
-#         surname = request.form.get('surname')
-#         middle_name = request.form.get('middle_name')
-#         group = request.form.get('group')
-#         student.create_students(name=name, surname=surname, middle_name=middle_name, group=group)
-#         data = student.get_all()
-#         return render_template("index.html", data=data)
-#     except AssertionError:
-#         return render_template('registration.html')
+@app.route('/video_feed')
+def video_feed():
+    #Video streaming route. Put this in the src attribute of an img tag
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/photo_feed',methods=['GET'])
+def photo_feed():
+    name = '1.jpg'
+    dir = app.root_path  + '\static'
+    return send_from_directory(
+        dir, name
+    )
+
+@app.route('/faceRecognized')
+def face_recognized():
+    print("ЛИЦО РАСПОЗНАНО")
+    return 0
+
+@app.route('/capture', methods=['POST', 'GET'])
+def capture():
+    # imageulr='https://i.ytimg.com/vi/lqYKkloAWMc/maxresdefault.jpg'
+    imageulr='https://i0.wp.com/spp-photo.ru/wp-content/uploads/2021/01/2-7.jpg'
+    my_dict = {
+    'streamId': '1',
+    'url': imageulr,
+    'left': 537,
+    'top': 438,
+    'width': 142,
+    'height': 156
+    }
+
+    json_str = json.dumps(my_dict)
+    print(json_str)
+
+    r = requests.post('http://localhost:9051/api/registerFace', data=json_str)
+    return r.content
 
 
 if __name__ == '__main__':
--- data/OperationsStudents.py
+++ data/OperationsStudents.py
@@ -12,33 +12,33 @@ port = "5432"
 #
 # cur = conn.cursor()
 
-class OperationsStudents:
-    def create_student(self, name, surname, middle_name, group):
-        conn = psycopg2.connect(database=db_name, user=user, password=password, host=host, port=port)
-        cur = conn.cursor()
-        try:
-            assert surname is not None
-            assert name is not None
-            assert middle_name is not None
-            assert group is not None
-            columns = ['name', 'surname', 'middle_name', 'group']
-            line_operators = ','.join(['%s' for _ in columns])
-            values = [name, surname, middle_name, group]
-            cur.execute(f'INSERT INTO student VALUES({line_operators})', values)
-            conn.commit()
-            conn.close()
-            return True
-        except:
-            return False
+# class OperationsStudents:
+#     def create_student(self, name, surname, middle_name, group):
+#         conn = psycopg2.connect(database=db_name, user=user, password=password, host=host, port=port)
+#         cur = conn.cursor()
+#         try:
+#             assert surname is not None
+#             assert name is not None
+#             assert middle_name is not None
+#             assert group is not None
+#             columns = ['name', 'surname', 'middle_name', 'group']
+#             line_operators = ','.join(['%s' for _ in columns])
+#             values = [name, surname, middle_name, group]
+#             cur.execute(f'INSERT INTO student VALUES({line_operators})', values)
+#             conn.commit()
+#             conn.close()
+#             return True
+#         except:
+#             return False
 
-    def get_all(self):
-        conn = psycopg2.connect(database=db_name, user=user, password=password, host=host, port=port)
-        cur = conn.cursor()
-        cur.execute('''SELECT surname, name, middle_name FROM student''')
-        data = cur.fetchall()
-        print(data)
-        formatted_data = [' '.join(item) for item in data]
+#     def get_all(self):
+#         conn = psycopg2.connect(database=db_name, user=user, password=password, host=host, port=port)
+#         cur = conn.cursor()
+#         cur.execute('''SELECT surname, name, middle_name FROM student''')
+#         data = cur.fetchall()
+#         print(data)
+#         formatted_data = [' '.join(item) for item in data]
 
-        cur.close()
-        conn.close()
-        return formatted_data
+#         cur.close()
+#         conn.close()
+#         return formatted_data
